b6991ceca530a64990d5870ce61ec4dde23161bf
Richard Guarnieri <richard.gm@outlook.com>
2022-06-15 22:04:12 -0400

    fixed user seed validation on bulkCreate

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,8 +3,11 @@ const User = require('./../models/User')
 const userSeedData = require('./userSeedData.json');
 
 const seedData = async () => {
-    // await sequelize.sync( {force: true});
-    await User.bulkCreate(userSeedData);
+    // await sequelize.sync();
+    await User.bulkCreate(userSeedData, {
+        validate: true,
+        individualHooks: true
+    });
     process.exit(0);
 }
 
