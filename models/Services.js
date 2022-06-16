ef5e1bcf1d1560cac923cad49cee8467aefd2a22
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-06-15 20:47:34 -0500

    services model

---
+++ models/Services.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('./../config/connection');
+
+class Service extends Model {};
+
+Service.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate: {
+                isDecimal: true,
+            },
+        },
+        active_status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        employee_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'empployee',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        timestamps: false,
+        underscored: true,
+        modelName: 'service'
+    }
+)
+
+module.exports = Service;
