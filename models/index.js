ac0ec602523656eeab6027f654a5b21173e05703
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-06-18 10:09:10 -0500

    Associations file

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Cities = require('./Cities');
+const Employee = require('./Employee');
+const services = require('./services');
+const Appointment = require('./Appointment');
