7a0c10c54837da7cb0b8d533891e8ea18596c94c
AngelMond <abraham_will@hotmail.com>
2022-06-18 12:56:26 -0500

    Just organizing the imports

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 // const routes = require('./routes');
+
+//Import all routes
+const routes = require('./routes')
+//Import sequalize conecction
 const sequelize = require('./config/connection');
-const routes = require('./routes/index')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
