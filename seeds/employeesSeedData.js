1d71970105c0dc7566a967dc5608b70130ad8f8c
AngelMond <abraham_will@hotmail.com>
2022-06-18 12:54:42 -0500

    Added seeds and bulkCreate function to employeesSeeds

---
+++ seeds/employeesSeedData.js
@@ -0,0 +1,32 @@
+const {Employee} = require('../models/Employee')
+
+const employeeData = [
+    {
+        "email": "prueba1@gmail.com",
+        "password": "12345",
+        "first_name": "Lalito",
+        "last_name": "Perez",
+        "citie_id": "1",
+        "available_schedule": ""
+    },
+    {
+        "email": "prueba2@gmail.com",
+        "password": "12345",
+        "first_name": "Chanchito",
+        "last_name": "Feliz",
+        "citie_id": "1",
+        "available_schedule": ""
+    },
+    {
+        "email": "prueba3@gmail.com",
+        "password": "12345",
+        "first_name": "Jorge",
+        "last_name": "Paredes",
+        "citie_id": "1",
+        "available_schedule": ""
+    }
+];
+
+const seedEmployee = () => Employee.bulkCreate(employeeData);
+
+module.exports = seedEmployee;
