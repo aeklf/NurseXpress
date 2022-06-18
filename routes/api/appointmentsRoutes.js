406c0ec4ba9eb149168ab6ebe09e7cf569618b04
Rembrandt <95456210+chrisnunez@users.noreply.github.com>
2022-06-18 11:55:23 -0700

    added appointment routes

--- routes/api/appointmentsRoutes.js
+++ routes/api/appointmentsRoutes.js
@@ -1,35 +1,9 @@
 const router = require('express').Router();
 const {Appointments, Employee, User, Cities} = require('../../models/Appointments');
 
-router.get('/appointments' , async (req, res) => {
+
     
-    Appointments.findAll({
-        attributes: ['id', 'date', 'hour',],
-        include: [
-            {
-            model: Employee,
-            attributes: ['first_name', 'last_name']
-            },
-
-            {
-            model: User,
-            attributes: ['first_name', 'last_name']
-            },
-            {
-            model: Cities,
-            attributes: ['']// needs location? not lat long
-            }
-    ]
-})
-.then(appointmentsData => res.json(appointmentsData))
-.catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-})
-})
-
 router.post('/appointments', async (req, res) => {
-    const { employee_id, user_id, date, hour, location} = req.body;
-    
+    Appointments.create()
 })
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const serviceRoutes = require('./servicesRoutes');
 const employeeRoutes = require('./employeesRoutes');
-const citiesRoutes = require('./citiesRoutes')
+const citiesRoutes = require('./citiesRoutes');
+const appointmentsRoutes = require('./appointmentsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/services', serviceRoutes);
 router.use('/employees', employeeRoutes);
 router.use('./cities', citiesRoutes);
+router.use('./appointments', appointmentsRoutes);
 
 module.exports = router;
