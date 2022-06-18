39948a99ee404d31f334ffcbee6be0b0f00ed6d6
AngelMond <abraham_will@hotmail.com>
2022-06-18 16:31:00 -0500

    Extension for userSeedData.json changed for .js and seedUser Function created

---
+++ seeds/userSeedData.js
@@ -0,0 +1,21 @@
+const User = require('../models/User')
+
+const userData = [
+    {
+        "email": "test2wqq22@test.com",
+        "password": "1232123121334",
+        "first_name": "John",
+        "last_name": "Doe"
+
+    },
+    {
+        "email": "test2wqd24@test.com",
+        "password": "1233131232134",
+        "first_name": "John",
+        "last_name": "Doe"
+    }
+];
+
+const seedUser = () => User.bulkCreate(userData);
+
+module.exports = seedUser;
