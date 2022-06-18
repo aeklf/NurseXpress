54f06a966523ed6f4eaceea608203867ed90349e
AngelMond <abraham_will@hotmail.com>
2022-06-18 12:57:42 -0500

    Adding seedEmployee and SeedCities to function seedData

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,8 @@
 const sequelize = require('./../config/connection');
 const User = require('./../models/User')
+const seedEmployee = require('./employeesSeedData')
+const seedCities = require('./citiesSeedData')
+
 const userSeedData = require('./userSeedData.json');
 
 const seedData = async () => {
@@ -8,6 +11,10 @@ const seedData = async () => {
         validate: true,
         individualHooks: true
     });
+    
+    await seedEmployee();
+    await seedCities();
+
     process.exit(0);
 }
 
