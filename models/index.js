9ff523ca6fae13d56ea65882fc5fed7ed58316c5
AngelMond <abraham_will@hotmail.com>
2022-06-20 21:28:40 -0500

    Setting references in sequelize-models/index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,41 @@
 const User = require('./User');
 const Cities = require('./Cities');
 const Employee = require('./Employee');
-const services = require('./services');
+const Services = require('./services');
 const Appointment = require('./Appointments');
+
+
+Employee.hasMany(Services, {
+    foreignKey: 'employee_id'
+});
+
+Employee.hasMany(Appointment, {
+    foreignKey: 'employee_id'
+});
+
+
+Services.belongsToMany(Employee)
+Employee.belongsToMany(Services)
+
+
+
+User.hasMany(Appointment, {
+    foreignKey:'user_id'
+});
+
+Appointment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Appointment.belongsTo(Employee, {
+    foreignKey: 'user_id'
+});
+
+
+
+
+Cities.hasMany(Services, {
+    foreignKey: 'employee_id'
+});
+
+
