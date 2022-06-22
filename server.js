fb550f76e1540ee9e8d294cf79100d373421f546
AngelMond <abraham_will@hotmail.com>
2022-06-22 18:44:55 -0500

    Installing handlebars and created folder views

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
+
+const hbs = exphbs.create({});
+
 
 //Import sequalize conecction
 const sequelize = require('./config/connection');
@@ -11,9 +15,12 @@ const routes = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+app.engine('handlebars',hbs);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public')));
 
 // turn on routes
 app.use(routes);
