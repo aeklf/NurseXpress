47e97dbf818346c50d3fae303a115582cd2c83c3
AngelMond <abraham_will@hotmail.com>
2022-06-22 19:21:13 -0500

    Fix connection to server and DB

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
+
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 router.use('*', async (req, res) => {
--- server.js
+++ server.js
@@ -2,25 +2,25 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 
-const hbs = exphbs.create({});
-
-
 //Import sequalize conecction
 const sequelize = require('./config/connection');
 
 //Import all routes
-const routes = require('./controllers');
+const routes = require('./controllers/index');
+
+const hbs = exphbs.create({});
+
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.engine('handlebars',hbs);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
