945a35808f470b24d957108bccd932900db5d4d1
AngelMond <abraham_will@hotmail.com>
2022-06-22 19:46:39 -0500

    Home route with handlebars done!

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res)=>{
-    res.render('otro');
+    res.render('home');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(routes);
 const init = async () => {
   try {
     await sequelize.query("SET FOREIGN_KEY_CHECKS = 0");
-    // await sequelize.sync({force:true});
+    await sequelize.sync({force:false});
     await sequelize.query("SET FOREIGN_KEY_CHECKS = 1");
     console.log('Successful connection to the database');
     app.listen(PORT, () => console.log('Express web server now listening'));
