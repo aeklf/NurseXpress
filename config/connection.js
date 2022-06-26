3bbd5b5bdfd97cc39acce273ae105b227e3b1d7d
Rembrandt <95456210+chrisnunez@users.noreply.github.com>
2022-06-25 21:11:29 -0700

    connection modified to url

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL,
+  'mysql://bac205d3a0a037:e1dc3303@us-cdbr-east-05.cleardb.net/heroku_616480eb9e4f468?reconnect=true',
   {
    
     dialect: 'mysql',
